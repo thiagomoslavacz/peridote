0356bda409ea2bee7f012cf1588d3cb2984b20af
thiagomoslavacz <thiagomoslavacz@gmail.com>
2024-04-13 23:02:29 -0300

    media-querie #3

--- script.js
+++ script.js
@@ -47,3 +47,12 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
