0cc780a3572cc703fdf48988a34338fd266cc61d
thiagomoslavacz <thiagomoslavacz@gmail.com>
2024-04-19 12:24:31 -0300

    Body Scroll Animation

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+// Header
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
+// Menu
+document.addEventListener("DOMContentLoaded", function () {
+  var menuButton = document.getElementById("menu");
+  var menuWindow = document.getElementById("menu-window");
+  var overlay = document.getElementById("overlay");
+  var closeButton = document.querySelector(".bx-x");
+  var navLinks = document.querySelectorAll(".nav li a");
+  var body = document.querySelector("body");
+
+  menuButton.addEventListener("click", function () {
+    menuWindow.classList.toggle("open");
+    overlay.style.display = "block";
+    body.style.overflow = "hidden";
+  });
+
+  closeButton.addEventListener("click", function () {
+    menuWindow.classList.remove("open");
+    overlay.style.display = "none";
+    body.style.overflow = "auto";
+  });
+
+  overlay.addEventListener("click", function () {
+    menuWindow.classList.remove("open");
+    overlay.style.display = "none";
+    body.style.overflow = "auto";
+  });
+
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+      menuWindow.classList.remove("open");
+      overlay.style.display = "none";
+      body.style.overflow = "auto";
+    });
+  });
+});
+
+// Mugs About
 document.addEventListener("DOMContentLoaded", function () {
   const arrowIcon = document.getElementById("arrow-icon");
   const wrapper = document.getElementById("wrapper");
@@ -16,43 +63,4 @@ document.addEventListener("DOMContentLoaded", function () {
 
     isWrapperOpen = !isWrapperOpen;
   });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  var menuButton = document.getElementById("menu");
-  var menuWindow = document.getElementById("menu-window");
-  var overlay = document.getElementById("overlay");
-  var closeButton = document.querySelector(".bx-x");
-  var navLinks = document.querySelectorAll(".nav li a");
-
-  menuButton.addEventListener("click", function () {
-    menuWindow.classList.toggle("open");
-    overlay.style.display = "block"; //
-  });
-
-  closeButton.addEventListener("click", function () {
-    menuWindow.classList.remove("open");
-    overlay.style.display = "none";
-  });
-
-  overlay.addEventListener("click", function () {
-    menuWindow.classList.remove("open");
-    overlay.style.display = "none";
-  });
-
-  navLinks.forEach(function (link) {
-    link.addEventListener("click", function () {
-      menuWindow.classList.remove("open");
-      overlay.style.display = "none";
-    });
-  });
-});
-
-window.addEventListener("scroll", function () {
-  var header = document.querySelector("header");
-  if (window.scrollY > 50) {
-    header.classList.add("scrolled");
-  } else {
-    header.classList.remove("scrolled");
-  }
 });
